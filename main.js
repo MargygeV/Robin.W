90c00293856152edcbc69ab88ce9847f7ed5e380
Nikita Trifonov <69072512+MargygeV@users.noreply.github.com>
2025-01-15 21:46:29 +0700

    Burger menu added

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-window.addEventListener('scroll', e => {
-	let navbar = document.getElementById('navbar').classList
-	let active_class = "header_scrolled"
+const header = document.getElementById('header').classList
+const menu = document.getElementById('header__nav')
+const burger = document.querySelector('.burger__btn')
+const body = document.querySelector('body')
 
-	if(pageYOffset > 400) navbar.add(active_class)
-	else navbar.remove(active_class)
-})
+const header_activeClass = "header_scrolled"
+const menu_activeClass = "menu-active"
+
+window.addEventListener('scroll', e => {
+	if(pageYOffset > 400) header.add(header_activeClass)
+	else header.remove(header_activeClass)
+});
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active')
+ 
+  if(header && menu)
+  {
+  	header.toggle(menu_activeClass)
+  	menu.classList.toggle(menu_activeClass)
+  	body.classList.toggle(menu_activeClass)
+  }  	
+});

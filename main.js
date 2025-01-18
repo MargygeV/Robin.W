3d52d615824375ff2a313871775d3a771538b2de
Nikita Trifonov <69072512+MargygeV@users.noreply.github.com>
2025-01-18 16:04:22 +0700

    Active navbar added

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
-const header = document.getElementById('header').classList
-const menu = document.getElementById('header__nav')
-const burger = document.querySelector('.burger__btn')
-const body = document.querySelector('body')
+const header = document.getElementById("header")
+const headerClassList = header.classList
+const menu = document.getElementById("header__nav")
+const burger = document.querySelector(".burger__btn")
+const bodyClassList = document.querySelector("body").classList
+const menuLinks = document.querySelectorAll(".header__list-item a")
+const targetSections = document.querySelectorAll(".target-section")
 
-const header_activeClass = "header_scrolled"
-const menu_activeClass = "menu-active"
+const headerActiveClass = "header_scrolled"
+const menuActiveClass = "menu-active"
 
-window.addEventListener('scroll', e => {
-	if(pageYOffset > 400) header.add(header_activeClass)
-	else header.remove(header_activeClass)
+const toggleMenu = () => {
+  headerClassList.toggle(menuActiveClass),
+  menu.classList.toggle(menuActiveClass),
+  burger.classList.toggle(menuActiveClass)
+};
+
+window.addEventListener("scroll", () => {
+  let e = pageYOffset;
+  e > 90 ? headerClassList.add(headerActiveClass) : headerClassList.remove(headerActiveClass)
+  
+  targetSections.forEach(( (t, n) => {
+    t.offsetTop - header.clientHeight <= e && (menuLinks.forEach((e => {
+      e.classList.contains(menuActiveClass) && e.classList.remove(menuActiveClass)
+    })),
+    menuLinks[n].classList.add(menuActiveClass))
+  }))
 });
 
-burger.addEventListener('click', () => {
-  burger.classList.toggle('active')
- 
-  if(header && menu)
-  {
-  	header.toggle(menu_activeClass)
-  	menu.classList.toggle(menu_activeClass)
-  	body.classList.toggle(menu_activeClass)
-  }  	
+burger.addEventListener("click", () => { 
+  toggleMenu(bodyClassList.toggle(menuActiveClass))
 });
+
+menuLinks.forEach((e => {
+  e.addEventListener("click", ( () => {
+    toggleMenu(bodyClassList.remove(menuActiveClass))
+  }))
+}));
